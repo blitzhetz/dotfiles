f085b28a5ba921874f54a70532de5e37f6792824
Slobodan Zivanovic <120286670+blitzhetz@users.noreply.github.com>
2024-05-31 23:26:25 +0200

    Updates

--- nvim/lua/blitzhetz/core/autocmds.lua
+++ nvim/lua/blitzhetz/core/autocmds.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     pattern = "*",
     callback = function()
         vim.highlight.on_yank({
-            timeout = 50,
+            timeout = 150,
             visual = true,
         })
     end,
